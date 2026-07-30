775a19f64835d37364482cba8d8fc05808eb736c
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2026-07-30 16:08:29 -0300

    chore: New repositorie

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+<<<<<<< HEAD
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        
+=======
+
+public class Main {
+    public static void main(String[] args) {
+
+>>>>>>> 71b637dfbf5d4fc4a4037ef39f15ce1d26877ab7
+    }
+}
